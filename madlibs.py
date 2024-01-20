47e986612f4c6f53f3fe5232acafd7ecaa34ffe7
yozora <128562373+louwisalfred@users.noreply.github.com>
2024-01-21 00:04:42 +0800

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+
+adj = input("Adjective: ").strip().capitalize()
+verb1 = input("Verb: ").strip().capitalize()
+verb2 = input("Verb: ").strip().capitalize()
+famous_person = input("Famous person: ").strip().capitalize()
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+i love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
