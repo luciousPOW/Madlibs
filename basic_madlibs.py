a7ca74865972ead2a1c35d03a54010b245e0c4a3
yozora <128562373+louwisalfred@users.noreply.github.com>
2024-01-21 00:02:27 +0800

    Add files via upload

---
+++ basic_madlibs.py
@@ -0,0 +1,10 @@
+while True:
+    name = input("Enter your channel: ").strip().capitalize()
+    noun2 = input("Enter emotion: ").strip().capitalize()
+
+    madlibs = f"Hey welcome to my {name}'s channel and i am {noun2} for you to be here"
+    print(madlibs)
+    
+    break
+
+print("Subscribe!")
